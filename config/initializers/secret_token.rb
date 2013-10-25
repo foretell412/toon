dc21b215bdc4b5372ce59e51762c8a149afd85df
nguyenthanhtung <tungnt4121991@gmail.com>
2013-10-25 07:58:15 +0000

    Generate a new Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toon::Application.config.secret_key_base = 'b33ac318bb4b586f673dfdcfd57f5c486af59288966a1c4888bb477db37cd989946fa601643dab32ee1d9975524e4ade29cb7eb2588ee31b5f51619c57fc5f59'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Toon::Application.config.session_store :cookie_store, key: '_toon_session'
